b94b55bdf190a6be450b8c7d13e89d87bda5909b
root <root@844e23823a62>
2020-12-03 02:38:30 +0000

    advanced robot race ip.

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,66 @@
 package _01_Olympic_Rings;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	static Robot ob = new Robot(340, 100);
+	static Robot rob = new Robot(140, 100);
+	static Robot rib = new Robot(190, 140);
+	static Robot rb = new Robot(240, 100);
+	static Robot ro = new Robot(290, 140);
 
+public static void main(String[] args) {
+	ob.penDown();
+	ob.hide();
+	rob.penDown();
+	rob.hide();
+	rib.penDown();
+	rib.hide();
+	rb.penDown();
+	rb.hide();
+	ro.penDown();
+	ro.hide();
+	Thread r1 = new Thread(() ->{
+		for (int i = 0; i < 360; i++) {
+		rob.turn(1); 
+	rob.move(1);
+	rob.setRandomPenColor();
+		}
+	});
+	Thread r2 = new Thread(()->{
+		for (int i = 0; i < 360; i++) {
+
+	rib.turn(1);
+	rib.move(1);
+	rib.setRandomPenColor();
+		}
+	});
+	Thread r3 = new Thread(()->{
+		for (int i = 0; i < 360; i++) {
+	rb.turn(1);
+	rb.move(1);
+	rb.setRandomPenColor();
+		}
+	});
+	Thread r4 = new Thread(()->{
+		for (int i = 0; i < 360; i++) {
+	ob.turn(1);
+	ob.move(1);
+	ob.setRandomPenColor();
+		}
+		});
+	Thread r5 = new Thread(()->{
+		for (int i = 0; i < 360; i++) {
+	ro.turn(1);
+	ro.move(1);
+	ro.setRandomPenColor();
+		}
+		});
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+	}
 }
-
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,48 @@
 package _02_Advanced_Robot_Race;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	static Robot ob = new Robot(340, 100);
+	static Robot rob = new Robot(140, 100);
+	static Robot rib = new Robot(190, 140);
+	static Robot rb = new Robot(240, 100);
+	static Robot ro = new Robot(290, 140);
+	public static void main(String[] args) {
+		Thread r1 = new Thread(() ->{
+			for (int i = 0; i < 360; i++) {
+			rob.turn(1); 
+		rob.move(1);
+		rob.setRandomPenColor();
+			}
+		});
+		Thread r2 = new Thread(()->{
+			for (int i = 0; i < 360; i++) {
+
+		rib.move(1);
+			}
+		});
+		Thread r3 = new Thread(()->{
+			for (int i = 0; i < 360; i++) {
+		rb.move(1);
+		}
+		});
+		Thread r4 = new Thread(()->{
+			for (int i = 0; i < 360; i++) {
+		ob.move(1);
+			}
+			});
+		Thread r5 = new Thread(()->{
+			for (int i = 0; i < 360; i++) {
+		ro.move(1);
+			}
+			});
+			r1.start();
+			r2.start();
+			r3.start();
+			r4.start();
+			r5.start();
+	}
 }
