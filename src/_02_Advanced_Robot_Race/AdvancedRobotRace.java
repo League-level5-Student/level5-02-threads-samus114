ee484c2627319c1c0d28d7781ceff444784a7737
root <root@f9d418b88c88>
2020-12-10 02:29:25 +0000

    advanced robot race ip

--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,48 +1,55 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
+	 // Re-do the robot race recipe from level 3 module 0.
 	// This time, use threads to make all of the robots go at the same time.
-	static Robot ob = new Robot(340, 100);
-	static Robot rob = new Robot(140, 100);
-	static Robot rib = new Robot(190, 140);
-	static Robot rb = new Robot(240, 100);
-	static Robot ro = new Robot(290, 140);
+	static boolean raceWon = false;
+	Thread[] threads;
+	static String winner = "";
+	static Random rand = new Random();
+	static int randNum = rand.nextInt(300);
 	public static void main(String[] args) {
-		Thread r1 = new Thread(() ->{
-			for (int i = 0; i < 360; i++) {
-			rob.turn(1); 
-		rob.move(1);
-		rob.setRandomPenColor();
+		Thread r1 = new Thread(() -> {
+			while (raceWon == false) {
+				randNum = rand.nextInt(300);
+				rob.move(randNum);
+				if(rob.getY() <= 100) {
+					raceWon = true;
+					winner = "rob";
+				}
 			}
 		});
-		Thread r2 = new Thread(()->{
-			for (int i = 0; i < 360; i++) {
-
-		rib.move(1);
-			}
-		});
-		Thread r3 = new Thread(()->{
-			for (int i = 0; i < 360; i++) {
-		rb.move(1);
-		}
-		});
-		Thread r4 = new Thread(()->{
-			for (int i = 0; i < 360; i++) {
-		ob.move(1);
-			}
-			});
-		Thread r5 = new Thread(()->{
-			for (int i = 0; i < 360; i++) {
-		ro.move(1);
-			}
-			});
-			r1.start();
-			r2.start();
-			r3.start();
-			r4.start();
-			r5.start();
+		r1.start();
 	}
+void setUpRace() {
+	threads = new Thread[5];
+	for(int i = 0; i < threads.length; i++) {
+		threads[i] = new Thread(new AdvancedRobot(i));
+	}
+		
+}
+public class AdvancedRobot implements Runnable {
+	Robot robot;
+	int botPos;
+	AdvancedRobot(int pos) {
+		robot = new Robot();
+		botPos = pos;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while (raceWon == false) {
+			randNum = rand.nextInt(300);
+			robot.move(randNum);
+			if(robot.getY() <= 100) {
+				raceWon = true;
+				System.out.println(botPos + "won the race!");
+			}
+		}
+	}
+}
 }
